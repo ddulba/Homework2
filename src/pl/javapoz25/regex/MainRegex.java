8a1255cb846f94226e876438ddb99e7c05e102b9
ddulba <darek.dulba@gmail.com>
2020-05-22 16:54:47 +0200

    Regex init

---
+++ src/pl/javapoz25/regex/MainRegex.java
@@ -0,0 +1,20 @@
+package pl.javapoz25.regex;
+
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class MainRegex {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        //Exercise 1
+        System.out.println("Exercise 1:");
+
+        System.out.print("Podaj ciąg znaków: ");
+        String stream = in.nextLine();
+
+
+    }
+}
